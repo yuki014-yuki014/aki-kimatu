e9cf09266f17acde12f4e52007d81513712879d2
yuki014-yuki014 <131091980+yuki014-yuki014@users.noreply.github.com>
2024-01-07 23:31:14 +0900

    Update 1-1.js

--- 1-1.js
+++ 1-1.js
@@ -1,3 +1,3 @@
 var img;
 function imgchange1(){document.getElementById('logo').src='./image/IMG_2682.jpeg';}
-function imgchange2(){document.getElementById('logo').src='';}
+function imgchange2(){document.getElementById('logo').src='./image/IMG_3401.jpeg';}
