091caf31188432e9c913d3ad7d67e2af00cb7e7d
yuki014-yuki014 <131091980+yuki014-yuki014@users.noreply.github.com>
2024-01-08 00:14:07 +0900

    Update 1-1.js

--- 1-1.js
+++ 1-1.js
@@ -1,3 +1,3 @@
 var img;
-function imgchange1(){document.getElementById('logo').src='./image/IMG_2682.jpeg';}
-function imgchange2(){document.getElementById('logo').src='./image/IMG_3401.jpeg';}
+function imgchange1(){document.getElementById('inu').src='./image/IMG_2682.jpeg';}
+function imgchange2(){document.getElementById('inu').src='./image/IMG_3401.jpeg';}
