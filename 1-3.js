2e11bdfda12ced168beb10ef83d2bfc505dabe41
yuki014-yuki014 <131091980+yuki014-yuki014@users.noreply.github.com>
2024-01-08 00:44:25 +0900

    Update 1-3.js

--- 1-3.js
+++ 1-3.js
@@ -1,1 +1,10 @@
-
+function nazo(){
+  if(document.getElementById('answer').value=='龍'){
+    alert('あたり！');}
+  else if(document.getElementById('answer').value=='りゅう'){
+    alert('あたり！');} 
+  else if(document.getElementById('answer').value=='リュウ'){
+    alert('あたり！');}  
+  else{
+    alert('はずれ！');}
+  }
