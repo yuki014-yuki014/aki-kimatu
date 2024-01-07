ba10cb26bc1b465fdfdd128dbafac3827cde1af7
yuki014-yuki014 <131091980+yuki014-yuki014@users.noreply.github.com>
2024-01-08 00:23:02 +0900

    Create 1-2.js

---
+++ 1-2.js
@@ -0,0 +1,8 @@
+function nazo(){
+  if(document.getElementById('answer').value=='柴犬'){
+    alert('あたり！');}
+  else if(document.getElementById('answer').value=='しばいぬ'){
+    alert('あたり！');} 
+  else{
+    alert('はずれ！');}
+  }
